0e2422377aaa5f1c0f410824f5f16ec67136248a
Nguyen Duc <97241208+n2duc@users.noreply.github.com>
2024-11-13 11:29:17 +0700

    update

---
+++ client/src/type/data-models.ts
@@ -0,0 +1,17 @@
+import { ROLE } from "./roles";
+
+export type User = {
+  _id: string;
+  email: string;
+  username: string;
+  role: ROLE;
+}
+
+export type Product = {
+  _id: string;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+  sizes: string[];
+}
