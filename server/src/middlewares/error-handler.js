de84d8177cfa605b2fbaba7811ceb96a4459b0ce
Nguyen Duc <97241208+n2duc@users.noreply.github.com>
2024-12-11 17:38:43 +0700

    fix: remove image

--- server/src/middlewares/error-handler.js
+++ server/src/middlewares/error-handler.js
@@ -2,7 +2,6 @@ import { StatusCodes } from "http-status-codes";
 
 // eslint-disable-next-line no-unused-vars
 const ErrorHandler = (err, req, res, next) => {
-  console.log("Middleware Error Handling");
   const errStatus = err.statusCode || 500;
   const errMsg = err.message || "Something went wrong";
   if (errMsg === "jwt expired") {
